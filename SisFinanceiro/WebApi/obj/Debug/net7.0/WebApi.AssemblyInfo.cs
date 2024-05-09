d4c6179bfb3003e3ff2b1d80e0b6d330b234b194
Anderson Silva <anderson.ssilva@outlook.com>
2024-05-09 18:05:48 -0300

    metodos cadastro usuarios

--- SisFinanceiro/WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
+++ SisFinanceiro/WebApi/obj/Debug/net7.0/WebApi.AssemblyInfo.cs
@@ -15,7 +15,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+d43d7d94867f9d75a180d828d9306a963e538c18")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+3e1003409380469c2dbfc48152c777884be82391")]
 [assembly: System.Reflection.AssemblyProductAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyTitleAttribute("WebApi")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
